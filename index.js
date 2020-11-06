dd6d2893ceab2c529f1d35c6e11f30471594f80b
Gabriel Cuenca Moncín <gcuencamoncin@gmail.com>
2020-11-06 10:55:17 +0100

    A little help to the backtracking

--- index.js
+++ index.js
@@ -4,23 +4,6 @@ const square = [
   [1, 8, 5]
 ]
 
-// [1,2,3,4,5,6,7,8,9]
-// [1,9]
-// [2,8]
-// [3,7]
-// [4,6]
-
-/**
- * First option: Divide the sum of all values of the square by n (being n the length of square)
- * Second option: Multiply the median of all values of the square by n (being n the length of square)
- */
-const getMagicSum = square => {
-  const sum = square.reduce((prev, curr) => {
-    return prev + curr.reduce((a, b) => a + b, 0)
-  }, 0)
-  return sum / square.length
-}
-
 /**
  * The median of all sorted values of the square
  */
@@ -30,58 +13,92 @@ const getCenter = square => {
   return arr.length % 2 == 0 ? (arr[mid] + arr[mid - 1]) / 2 : arr[mid - 1]
 }
 
-const initMap = keys => {
-  const map = new Map()
-  keys.forEach(i => {
-    map.set(i, false)
-  })
-  return map
+const checkSolution = square => {
+
+  // Check rows
+  for (let i = 0; i < 3; i++) {
+    const sum = square[i].reduce((a, b) => a + b, 0)
+    if (sum !== 15)
+      return false
+  }
+
+  // Check columns using transpose
+  let copySquaure = [...square]
+  copySquaure = copySquaure[0].map((col, i) => copySquaure.map(row => row[i]))
+  for (let i = 0; i < 3; i++) {
+    const sum = copySquaure[i].reduce((a, b) => a + b, 0)
+    if (sum !== 15)
+      return false
+  }
+
+  // Check first diagonal
+  let sumDiagonals = 0
+  for (let i = 0; i < 3; i++) {
+    sumDiagonals += square[i][i]
+  }
+  if (sumDiagonals !== 15)
+    return false
+
+  // Check second diagonal
+  sumDiagonals = 0
+  for (let i = 0; i < 3; i++) {
+    sumDiagonals += square[i][square.length - 1 - i]
+  }
+  if (sumDiagonals !== 15)
+    return false
+
+  return true
+
 }
 
-const checkArr = arr => {
-  const magicSum = getMagicSum(square)
-  return magicSum === arr.reduce((a, b) => a + b, 0)
-}
-
-const magic = (map, arr, center) => {
-  console.log(map)
-}
-
-const getPairs = (arr, pairs = []) => {
-  if (arr.length <= 1)
-    return pairs
-  else {
-    const first = arr.shift()
-    const last = arr.pop()
-    pairs.push([first, last])
-    return getPairs(arr, pairs)
+const backtracking = square => {
+  for (let i = 0; i <= 9; i++) {
+    let row = Math.floor(i / 3)
+    let column = i % 3
+    if (row > 2) {
+      break
+    }
+    if (square[row][column] === 0) {
+      for (let value = 1; value <= 9; value++) {
+        if (!(square[0].includes(value) || square[1].includes(value) || square[2].includes(value))) {
+          square[row][column] = value
+          // console.log(square)
+          if (checkSolution(square)) {
+            console.log('solution')
+            return true
+          } else {
+            if (backtracking(square)) {
+              return true
+            }
+          }
+        }
+      }
+      square[row][column] = 0
+    }
   }
 }
 
-const main = square => {
+const mainx = square => {
+  if (!checkSolution(square)) {
+    const center = getCenter(square)
+    const _square = [
+      [0, 0, 0],
+      [0, center, 0],
+      [0, 0, 0]
+    ]
+    const solved = backtracking(_square)
+    if (solved) {
+      console.log(_square)
+    } else {
+      console.log('Solution not found')
+    }
+  } else {
+    console.log(square)
+  }
 
-  // IF square is magic then finish the program
-  //
-  // ELSE
-  //
-
-  const arr = square.flat().sort()
-  const pairs = getPairs([...arr])
-
-  const center = getCenter(square)
-
-  const centerRow = [pairs[0][0], center, pairs[0][1]]
-  pairs.shift()
-  console.log(pairs)
-  const upperRow = [undefined, undefined, undefined]
-  const lowerRow = [undefined, undefined, undefined]
-
-
-  // const map = initMap(arr)
-  // magic(map, arr, center)
 }
 
-const magicSquare = main(square)
+mainx(square)
 
 
 module.exports = {}
